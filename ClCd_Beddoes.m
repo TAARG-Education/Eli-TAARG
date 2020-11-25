a55d3c8a6520f56c87d9ea682614fbcccdc73167
Luciano <Luciano@DESKTOP-M6FS84D.station>
2020-11-25 16:54:42 +0100

    Cl-Cd function (Beddoes Method) created.

---
+++ ClCd_Beddoes.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ClCd_Beddoes(inputArg1,inputArg2)
+%CLCD_BEDDOES Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
