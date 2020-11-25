fe330d56cf264a11c67b9c8a38486c4e4e6384ea
CrescenzoVisone <cres.visone@studenti.unina.it>
2020-11-25 12:05:32 +0100

    Branch and fcn creation

---
+++ ClCd_XRotor.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ClCd_XRotor(inputArg1,inputArg2)
+%CLCD_XROTOR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
