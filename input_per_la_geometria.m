0de728fa67358f9c7d72a7c9ab8d6b325e1bf7a8
carmd <carmd@DESKTOP-H7VJIQS.homenet.telecomitalia.it>
2020-11-25 21:58:41 +0100

    funzione creata

---
+++ input_per_la_geometria.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = input_per_la_geometria(inputArg1,inputArg2)
+%INPUT_PER_LA_GEOMETRIA Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
