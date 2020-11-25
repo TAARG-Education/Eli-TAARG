cbcb7e72f40a7a1731b3cb2e1a92988360801720
carde <carde@LAPTOP-1UAJ4GJF.homenet.telecomitalia.it>
2020-11-25 21:57:41 +0100

    Creazione della Funzione

---
+++ FunzionidiOutput.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = FunzionidiOutput(inputArg1,inputArg2)
+%FUNZIONIDIOUTPUT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
