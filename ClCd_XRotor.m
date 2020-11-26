9551e002d4c0298b90be4a09c31a771349b41751
CrescenzoVisone <cres.visone@studenti.unina.it>
2020-11-26 12:08:43 +0100

    First draft of the ClCd_XRotor.m fcn

--- ClCd_XRotor.m
+++ ClCd_XRotor.m
@@ -1,7 +1,21 @@
-function [outputArg1,outputArg2] = ClCd_XRotor(inputArg1,inputArg2)
-%CLCD_XROTOR Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function [Cl_vec, Cd, Cd_bp] = ClCd_XRotor(Cd_min, dCd_dCl2, Cl_Cd_min, Re_ref, Re_inf, f, Cl_max, Cl_min, Cl_bp)
+    
+    Cl_vec = Cl_min:.01:Cl_max;
 
+    for i = 1:length(Cl_vec)
+        Cl = Cl_vec(i);
+        Cd(i) = (Cd_min + dCd_dCl2*(Cl_Cd_min - Cl)^2)*(Re_inf/Re_ref)^f;
+    end
+
+    % plot
+    plot(Cd, Cl_vec);
+    
+    %% evaluation of Cd values @ requested Cl breakpoints 
+    % note that Cl_bp can be both a single value or a vector
+    % definition of an anonymous function through the handle (@) symbol
+    Cd_interp = @(Cl_interp) interp1(Cl_vec, Cd, Cl_interp, 'pchip');
+    % anonymous fcn used to compute requested Cd
+    Cd_bp = Cd_interp(Cl_bp);
+
+
+end
