91e358d642959b0b77c847ea66017a4d3d9a526a
Serena <Serena@DESKTOP-807HI35.lan>
2020-12-01 22:07:15 +0100

    Creazione funzione 01/12/2020

---
+++ Angolo_Attacco_Effettivo.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Angolo_Attacco_Effettivo(inputArg1,inputArg2)
+%ANGOLO_ATTACCO_EFFETTIVO Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
