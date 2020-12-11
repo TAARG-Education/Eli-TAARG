1abec7847992547df27800c0cfd65472e868491f
franc <franc@DESKTOP-RQIK4V0.station>
2020-12-11 12:15:39 +0100

    The first release has been committed. In this version it is possibile to generate a data sheet (with adimensional radius, chord distribution, pitch angle distribution and thrust distribution) for an optimum propeller of given geometry, aerodynamic and thrust. This script generates plots of the main propeller characteristics. User interface and power option has not been impelmented yet. In this release, geometry and aerodynamic characteristics have to be provided by the user; further releases will include links to geometry and aerodynamic generating functions.

--- Opti_Prop.m
+++ Opti_Prop.m
@@ -2,7 +2,7 @@
 %  \brief: this function designs the geometry of an optimum propeller
 %  at given thrust or power.
 %  \authors : Francesco Gervasio, Nicola Russo
-%  \version: 1.01
+%  \version: 1.03
 %
 % Eli-TAARG is free software; you can redistribute it and/or
 % modify it under the terms of the GNU General Public
@@ -56,16 +56,16 @@
 clear all; close all; clc;
 
 %% INPUT
-R_prop  = 0.454;    %[m] 
+R_tip  = 0.95;     %[m] 
 N_blade = 2;        %[ ]
-R_hub   = 0.07;     %[m]
-A = pi * R_prop^2;  %[m]^2
-D_prop = R_prop*2;  %[m]
-n_rpm   = 2000;     %[rpm]
-V_inf   = 27.7 ;    %[m/s]
-T = 150000;         %[N]
-P = 500;            %[kW]
-h = 2000;           %[m]
+R_hub   = 0.14;     %[m]
+A = pi * R_tip^2;   %[m]^2
+D_prop = R_tip*2;   %[m]
+n_rpm   = 2500;     %[rpm]
+V_inf   = 45 ;    %[m/s]
+T = 964;            %[N]
+P = 964;            %[kW]
+h = 4000;           %[m]
 
 %% CONVERSIONS
 %{
@@ -80,8 +80,7 @@ n_rads = convangvel(n_rpm,'rpm','rad/s');
 The aerospace toolbox is required in order to calculate the ISA parameters.
 It can be downloaded from the Mathworks website.
 %}
-[T a_chi p rho] = atmosisa(h);
-
+[Temp a p rho] = atmosisa(h);
 %% Ct_Cp
 %{
 Given the required thrust or the required power, the related coefficients
@@ -101,24 +100,20 @@ eqn2 = T == 2*rho*A*(V_inf+w0)*w0;
 eqn = [eqn1 eqn2];
 S = solve(eqn,w0,'ReturnConditions',true);
 w_0 = double(S.w0);
+T_corr = 0;
 
+while (T-T_corr)/T >0.001
 %% Step 2
 %{
 The inflow angle distribution in already determined. A plot of this
 parameter has been provided as well.
 %}
-r = linspace(R_hub,R_prop,1000);
-r_adim = r/R_prop;
-phi = atan((V_inf+w_0)./(n_rads*r))';
+r = linspace(R_hub,R_tip,1000);
+r_adim = r/R_tip;
+phi = atan((V_inf+w_0)./(n_rads*r));
 phi_adim = atan((V_inf+w_0)./(n_rads*r_adim))';
-figure
-plot(r_adim,phi_adim,'-k');
-xlabel('$\bar{r}$','interpreter','latex');
-ylabel('$\phi$ (rad)','interpreter','latex');
-title('Inflow angle $ \phi$  vs $\bar{r}$','interpreter','latex');
-grid on;
 
-%% Step 3
+%% Step 3.1
 %{
 In this step a calculation of:
 - chi = (OMEGA*r)/V_inf (an alternative non-dimensional radius)
@@ -129,15 +124,8 @@ has been provided and plotted.
 chi = (n_rads.*r)/V_inf;
 a_chi = w_0/V_inf*((chi.^2)./((1+w_0/V_inf)^2+(chi.^2)));
 a_first_chi = w_0/V_inf*((1+w_0/V_inf)./((1+w_0/V_inf)^2+chi.^2));
-a_radim = w_0/V_inf*(((n_rads*r_adim*R_prop/V_inf).^2)./((1+w_0/V_inf)^2+((n_rads*r_adim*R_prop/V_inf).^2)));
-a_first_radim = w_0/V_inf*((1+w_0/V_inf)./((1+w_0/V_inf)^2+(n_rads*r_adim*R_prop/V_inf).^2));
-figure
-plot(chi,a_chi,'-.k')
-hold on
-plot(chi,a_first_chi,'-k')
-xlabel('$\chi$','interpreter','latex');
-legend('$a$','$a^{''}$','interpreter','latex');
-title('Axial induction $ a$ and rotational induction $a^{''}$ vs $\bar{r}$','interpreter','latex');
+a_radim = w_0/V_inf*(((n_rads*r_adim*R_tip/V_inf).^2)./((1+w_0/V_inf)^2+((n_rads*r_adim*R_tip/V_inf).^2)));
+a_first_radim = w_0/V_inf*((1+w_0/V_inf)./((1+w_0/V_inf)^2+(n_rads*r_adim*R_tip/V_inf).^2));
 
 %{
 rotational and flight speed velocity have been composed at any given
@@ -145,13 +133,6 @@ section in order to obtain the true velocity. A plot has been provided as...
 well 
 %}
 V_eff = (V_inf+w_0)./sin(phi);
-figure
-plot(r_adim,V_eff,'-k');
-xlabel('$\bar{r}$','interpreter','latex');
-ylabel('$V_{eff}$ (m/s)','interpreter','latex');
-title('Velocity $V_{eff}$  vs $\bar{r}$','interpreter','latex');
-grid on;
-hold on;
 
 
 %{
@@ -159,9 +140,80 @@ The Prandtl correction function for finite blade number has been
 implemented. Lambda is  = chi^-1. According to the momentum theory: ...
 w_j = 2*w_0;
 %}
-lambda = (n_rads*R_prop/V_inf)^-1;
+lambda = (n_rads*R_tip/V_inf)^-1;
 w_j = 2*w_0;
 F   = (2/pi)*acos(exp((N_blade/(2*lambda))*(r_adim-1)));
+
+%{
+The non-dimensional aerodynamic load gamma_adim_opti, already scaled with...
+the Prandtl correction function, can be plotted as well. The non ...
+dimensional Gamma function can be visualized in the aforementioned plot...
+ as ratio.
+%} 
+
+GAMMA = (4*pi.*F.*r_adim.^2.*a_first_radim*n_rads*R_tip^2)/N_blade';
+gamma_adim_opti = (N_blade.*GAMMA*n_rads)/(4*pi*V_inf*w_0);
+
+
+%% Step 3.2
+%{
+At each step, dT/dr_adim or dP/dr_adim are calculated. The optimal
+distribution is supposed to be linear. These values are lately integrated
+along the non dimensional radius in order to calculate a step T or P ...
+value that must be confronted with the design T or P value.
+%}
+dr_adim = gradient(r_adim);
+dT = N_blade*rho*(R_tip^2)*n_rads*r_adim.*(1-a_first_radim).*GAMMA.*dr_adim;
+Th = trapz(dT);
+
+%% Step 4
+sigma_cl = (1/pi)*((N_blade.*GAMMA)./(V_eff.*r_adim*R_tip));
+cl =  [0.8 0.8 0.8 0.6 0.4 ];
+cd =  [100 100 100 100 100 ]./10000;
+r_1=  [0.2 0.4 0.6 0.8 0.9 ];
+mu = 1e-5;
+c = 1;
+Re =  (rho.*V_eff*c)/mu;
+RE = [Re(200) Re(400) Re(600) Re(800) Re(900) Re(1000)];
+alpha = [2.8 2.79 2.77 1 -2.45]/57.63;
+alpha_int = @(t)interp1(r_1,alpha,t,'spline','extrap');
+cl_int = @(t)interp1(r_1,cl,t,'spline','extrap');
+cd_int = @(t)interp1(r_1,cd,t,'spline','extrap');
+t = linspace(0,1,1000);
+alpha_vec = alpha_int(t);
+cl_vec = cl_int(t);
+cd_vec = cd_int(t);
+theta = phi+alpha_vec;
+
+% calcolo delle perdite viscose
+lambda_first = cl_vec.*cos(phi)-cd_vec.*sin(phi);
+
+% calcolo della solidità
+sigma_radim = sigma_cl./cl_vec;
+chord = R_tip.*((2*pi.*r_adim.*sigma_radim)/N_blade);
+
+
+%% Step 5
+%Calcolo della spinta corretta
+dT_corr = rho*pi*sigma_radim.*lambda_first.*r_adim.*(V_eff.^2).*dr_adim;
+T_corr = trapz(dT_corr);
+w_0 = w_0 +0.01;
+end
+
+figure
+plot(r_adim,phi_adim,'-k');
+xlabel('$\bar{r}$','interpreter','latex');
+ylabel('$\phi$ (rad)','interpreter','latex');
+title('Inflow angle $ \phi$  vs $\bar{r}$','interpreter','latex');
+grid on;
+
+figure
+plot(chi,a_chi,'-.k')
+hold on
+plot(chi,a_first_chi,'-k')
+xlabel('$\chi$','interpreter','latex');
+legend('$a$','$a^{''}$','interpreter','latex');
+title('Axial induction $ a$ and rotational induction $a^{''}$ vs $\bar{r}$','interpreter','latex');
 figure
 plot(r_adim,F,'-k')
 xlabel('$\bar{r}$','interpreter','latex');
@@ -170,56 +222,36 @@ title('Prandtl correction function for finite blade number vs $\bar{r}$',...
     'interpreter','latex');
 grid on;
 
-%{
-The non-dimensional aerodynamic load gamma_adim_opti, already scaled with the Prandtl
-correction function, can be plotted as well. The non dimensional Gamma function can be
-visualized in the aforementioned plot as ratio.
-%} 
-GAMMA = (4*pi.*F.*r_adim.^2.*a_first_radim*n_rads*R_prop^2)/N_blade;
-gamma_adim_opti = (N_blade.*GAMMA*n_rads)/(4*pi*V_inf*w_0);
 figure
 plot(chi,gamma_adim_opti,'-k')
 grid on;
 xlabel('$\chi$','interpreter','latex');
 legend('$\frac{N \Gamma \Omega}{4\pi w_{0}*v_{inf}}$','interpreter','latex');
 
+figure
+plot(r_adim,theta,'-k');
+xlabel('$\bar{r}$','interpreter','latex');
+ylabel('$\theta$ (rad)','interpreter','latex');
+title('Theta angle $ \theta$  vs $\bar{r}$','interpreter','latex');
+grid on;
 
-%{
-At each step, dT/dr_adim or dP/dr_adim are calculated. The optimal
-distribution is supposed to be linear. These values are lately integrated
-along the non dimensional radius in order to calculate a step T or P value that
-must be confronted with the design T or P value.
-%}
+figure
+plot(r_adim,chord,'-k');
+grid on;
+xlabel('$\bar{r}$','interpreter','latex');
+ylabel('$c(\bar{r})$','interpreter','latex');
+title('Chord distribution c vs $\bar{r}$','interpreter','latex');
 
+figure
+plot(r_adim,dT_corr,'-k');
+grid on;
+xlabel('$\bar{r}$','interpreter','latex');
+ylabel('$\frac{dT}{d\bar{r}}$','interpreter','latex');
+title('Thrust distribution $T$ vs $\bar{r}$','interpreter','latex');
 
-sigma_cl = (1/pi)*((N_blade.*GAMMA)./(V_eff'.*r_adim.*R_prop));
-
-% %% Step 3
-% cl =  [0.8 0.8 0.8 0.6 0.2 0];
-% cd =  [100 100 100 100 100 0]./10000;
-% r  =  [0.2 0.4 0.6 0.8 0.9 1];
-% mu = 1e-5;
-% c = 1;
-% Re =  (rho.*V_eff*c)/mu;
-% RE = [Re(200) Re(400) Re(600) Re(800) Re(900) Re(1000)];
-% alpha = [2.8 2.79 2.77 1 -2.45 -4.19]/57.63;
-% alpha_int = @(t)interp1(r,alpha,t);
-% cl_int = @(t)interp1(r,cl,t);
-% cd_int = @(t)interp1(r,cd,t);
-% 
-% t = linspace(0.20,1,1000);
-% alpha_vec = alpha_int(t);
-% cl_vec = cl_int(t);
-% cd_vec = cd_int(t);
-% theta = phi(201:1000)+alpha_vec(201:1000);
-% 
-% %% Calcolo perdite viscose
-% 
-% lambda_first = cl_vec.*cos(phi')-cd_vec.*sin(phi');
-% 
-% dT_dr_adim = (0.5*sigma_cl.*r_adim.^2).*lambda_first;
-% T_star = trapz(dT_dr_adim(2:1000));
-% figure
-% plot(r_adim,dT_dr_adim);
-
-
+DATA(:,1) = r_adim';
+DATA(:,2) = theta';
+DATA(:,3) = chord';
+DATA(:,4) = dT_corr';
+filename = ['Data_Opti_Prop.txt'];
+dlmwrite(filename,DATA,'delimiter','\t','precision',4)
