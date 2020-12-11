563ea8acfd5daa34cd576b082b1630da98e2850d
Massimiliano <Massimiliano@LAPTOP-JT0Q54HD.lan>
2020-12-11 19:14:09 +0100

    Name fixed

--- Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello.m
+++ Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = Feature_Elica_intubata_Calcolo_intensita_adimensionale_vortice_ad_anello(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello(inputArg1,inputArg2)
 %FEATURE_ELICA_INTUBATA_CALCOLO_INTENSITÀ_ADIMENSIONALE_VORTICE_AD_ANELLO Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
