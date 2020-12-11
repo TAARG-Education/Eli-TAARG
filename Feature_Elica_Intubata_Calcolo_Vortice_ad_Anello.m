8d6d06894e1da5b2f39a2f0a79fc90c455cbc721
Massimiliano <Massimiliano@LAPTOP-JT0Q54HD.lan>
2020-12-11 09:49:44 +0100

    Funzione rinominata perchè ecceddeva il numero di caratteri

---
+++ Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Feature_Elica_intubata_Calcolo_intensita_adimensionale_vortice_ad_anello(inputArg1,inputArg2)
+%FEATURE_ELICA_INTUBATA_CALCOLO_INTENSITÀ_ADIMENSIONALE_VORTICE_AD_ANELLO Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
--- Feature_Elica_intubata_Calcolo_intensità_adimensionale_vortice_ad_anello.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = Feature_Elica_intubata_Calcolo_intensità_adimensionale_vortice_ad_anello(inputArg1,inputArg2)
-%FEATURE_ELICA_INTUBATA_CALCOLO_INTENSITÀ_ADIMENSIONALE_VORTICE_AD_ANELLO Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
