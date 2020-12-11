b12d6227d88fdabe069f264405cb828cd24fe61d
Luciano <Luciano@DESKTOP-M6FS84D.station>
2020-12-11 13:14:28 +0100

    function Turbine Darrieus created.

---
+++ feature_Turbine_Darrieus.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = feature_Turbine_Darrieus(inputArg1,inputArg2)
+%FEATURE_TURBINE_DARRIEUS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
