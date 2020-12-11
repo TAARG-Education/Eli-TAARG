364ffd5db5d8c29c47919b7412b5cbb3ef03f8ed
CamillaScotto <camillascottodi@outlook.it>
2020-12-11 14:04:53 +0100

    New turbine_Darrieus_tubo_flusso_multiplo function created.

---
+++ turbine_Darrieus_tubo_flusso_multiplo.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = turbine_Darrieus_tubo_flusso_multiplo(inputArg1,inputArg2)
+%TURBINE_DARRIEUS_TUBO_FLUSSO_MULTIPLO Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
