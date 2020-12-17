83fe4685a97c22cfab2ff805f67f7245f02ff671
peppe <peppe@DESKTOP-V9MCEIH>
2020-12-17 17:51:19 +0100

    Creazione funzione

---
+++ Curve_caratteristiche_aeromotori_asse_orizzontal.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = Curve_caratteristiche_aeromotori_asse_orizzontal(inputArg1,inputArg2)
+%CURVE_CARATTERISTICHE_AEROMOTORI_ASSE_ORIZZONTAL Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+
+outputArg2 = inputArg2;
+end
+
