75c2b9ceed598250a3ad396648f9515b242de64c
Teresa <you@example.com>
2020-12-17 19:07:44 +0100

    Aggiunta dell'header

--- input_per_la_geometria.m
+++ input_per_la_geometria.m
@@ -1,6 +1,44 @@
 function [outputArg1,outputArg2] = input_per_la_geometria(inputArg1,inputArg2)
 %INPUT_PER_LA_GEOMETRIA Summary of this function goes here
-%   Detailed explanation goes here
+%% \file nomefile
+%  \brief brevissima descrizione della funzione
+%  \author nome dell'autore
+%  \version versione
+%
+% Eli-TAARG is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public
+% License as published by the Free Software Foundation; either
+% version 3 of the License, or (at your option) any later version.
+%
+% Eli-TAARG is developed by the TAARG Educational organization for
+% educational purposes only.
+% Theoretical and Applied Aerodynamic Research Group - University of Naples Federico II.
+%
+% Eli-TAARG GitHub link: <https://github.com/TAARG-Education/Eli-TAARG>
+%
+% Eli-TAARG is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% General Public License for more details.
+% <http://www.gnu.org/licenses/>.
+%
+% ==============================================================================================
+% |Name        : Function for the propeller analysys input data                                 |
+% |Author      : Carmen Di Mauro & Teresa Aliperta                                              |
+% |              University of Naples Federico II.                                              |
+% |Version     : 1.0.0                                                                           |
+% |Date        : 25/11/2020                                                                     |
+% |Modified    : 15/12/2020                                                                     |
+% |Description : starting from a database the function provides different variables about the   | 
+%                propeller geometry that can be needed for different analysis                   |
+% |Reference   :                                                                                |
+% |Input       : Function for the reading of the propeller geometry that from a file .txt reads |
+%                the geometry of the propeller                                                  |
+% |Output      : variables that contain the geometry information needed                         |
+% |Note        :                                                                                |
+% ==============================================================================================
+
+
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
