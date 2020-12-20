a00ec191839334f236e9ca1fd55ae1ae207385ac
umber <umber@LAPTOP-60CQBBFC.home-life.hub>
2020-12-20 18:03:54 +0100

    Creazione della funzione

---
+++ BEMT.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = BEMT(inputArg1,inputArg2)
+%BEMT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
