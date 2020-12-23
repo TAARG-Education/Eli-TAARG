37d5c1b0fa39a05b58b8770446e31bd866ab06a2
Luciano <Luciano@DESKTOP-M6FS84D.station>
2020-12-23 13:15:53 +0100

    Updated 'Turbine_Darrieus_tubo_flusso_multiplo' function.

--- turbine_Darrieus_tubo_flusso_multiplo.m
+++ turbine_Darrieus_tubo_flusso_multiplo.m
@@ -11,7 +11,7 @@ ind = zeros(numel(phiv),numel(phiv));
 %% Calcolo di a, CP, CQ
 for j = 1 : numel(lambdav)
     lambda = lambdav(j); %assegnazione di lambda
-for i = 1 : numel(phiv) 
+    for i = 1 : numel(phiv) 
     phi = phiv(i); %assegnazione di phi
     eq = @(a) ((1-a).*a) - (c/(4*R))* ( (lambda + (1-a).*sin(phi)).^2 +...
          (1-a).^2.* cos(phi).^2).*cla.*(atan((1-a).*cos(phi))./(lambda + ...
@@ -20,36 +20,42 @@ for i = 1 : numel(phiv)
           a(1,i) = (fzero(eq,0.2)); % zero della funzione, per ogni phi abbiamo un valore di a --> a è costante lungo l'ala ma varia con phi
           ind(j,:) = a; %lungo la riga varia con phi, lungo la colonna varia con lambda
 
-v_vinf(j,:) = sqrt((lambda + (1-a).*sin(phiv)).^2 + (1-a).^2 .*cos(phiv).^2); %v/vinf: lungo le righe varia con phi, lungo le colonne varia con lambda
-alpha(j,:) = atan2(((1-a).*cos(phiv)),(lambda + (1-a).*sin(phiv))); %alpha: lungo le righe varia con phi, lungo le colonne varia con lambda
-cost_p = (N*c*lambda)./(4*pi*R); % costante nel calcolo dell'integrale (cp)
-cp(j,1) = cost_p.* trapz(phiv, (v_vinf(j,:).^2*cla.*alpha(j,:).*sin(alpha(j,:)).*(1-(cd./(cla.*alpha(j,:))).*cot(alpha(j,:))))); %calcolo di cp
-cq(j,1) = cp(j,1)/lambda; %9.6 pagina 118 %calcolo cq
-end
+    v_vinf(j,:) = sqrt((lambda + (1-a).*sin(phiv)).^2 + (1-a).^2 .*cos(phiv).^2); %v/vinf: lungo le righe varia con phi, lungo le colonne varia con lambda
+    alpha(j,:) = atan2(((1-a).*cos(phiv)),(lambda + (1-a).*sin(phiv))); %alpha: lungo le righe varia con phi, lungo le colonne varia con lambda
+    cost_p = (N*c*lambda)./(4*pi*R); % costante nel calcolo dell'integrale (cp)
+    cp(j,1) = cost_p.* trapz(phiv, (v_vinf(j,:).^2*cla.*alpha(j,:).*sin(alpha(j,:)).*(1-(cd./(cla.*alpha(j,:))).*cot(alpha(j,:))))); %calcolo di cp
+    cq(j,1) = cp(j,1)/lambda; %9.6 pagina 118 %calcolo cq
+    end
 
 massimi = zeros(numel(lambdav),1);
 alphadeg = rad2deg(alpha); % dominio di phi in rad
 
-for h = 1 : numel(phiv)
-    massimi(h) = max(alphadeg(h,:));
-end
+    for h = 1 : numel(phiv)
+        massimi(h) = max(alphadeg(h,:));
+    end
 
 contatorelambdamin = 0;
 
-for f = 1 : numel(phiv)
+    for f = 1 : numel(phiv)
         if alphamax < massimi(f)
             contatorelambdamin = contatorelambdamin + 1;
         end
-end
+    end
 
 contatorelambdamax = 0;
 
-for k = 1 : length(phiv)
-    if cp(k) < 0 
+    for k = 1 : length(phiv)
+        if cp(k) < 0 
         contatorelambdamax = contatorelambdamax + 1;
+        end
     end
-end
 
 end
+% cq e cp vengono ridimensionati considerando il
+% lambdaminimo(alpha deve essere minore dell'alphadistallo) e il lambdamassimo(cp>0)
+cq = cq(contatorelambdamin:end-contatorelambdamax,1);
+cp = cp(contatorelambdamin:end-contatorelambdamax,1);
+end
+
 
 
