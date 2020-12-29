9bc3cc28d1266b0933a748c912535a2376e15e58
david <david@LAPTOP-8G1I5D4U.homenet.telecomitalia.it>
2020-12-29 12:42:09 +0100

    Header has been added.

--- geometryreader.m
+++ geometryreader.m
@@ -1,6 +1,45 @@
-%----------------------------------------------------------------------
-% Aerodinamica dell'ala rotante - Classe per la lettura della geometria
-%----------------------------------------------------------------------
+%% \file funzione per la lettura della geometria
+%  \brief the function reads the geometry of a propeller and analyzes it   
+%         using xrotor
+%  \author Davide Mennella & Alessandro Carmine Esposito
+%  \version 1.0
+%
+% Eli-TAARG is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public
+% License as published by the Free Software Foundation; either
+% version 3 of the License, or (at your option) any later version.
+%
+% Eli-TAARG is developed by the TAARG Educational organization for
+% educational purposes only.
+% Theoretical and Applied Aerodynamic Research Group - University of Naples Federico II.
+%
+% Eli-TAARG GitHub link: <https://github.com/TAARG-Education/Eli-TAARG>
+%
+% Eli-TAARG is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% General Public License for more details.
+% <http://www.gnu.org/licenses/>.
+%
+% ==============================================================================================
+% |Name        : Function for the reading of the propeller geometry                            |
+% |Author      : Davide Mennella& Alessandro Carmine Esposito                                  |
+% |              University of Naples Federico II.                                             |
+% |Version     : 1.0                                                                           |
+% |Date        : 16/11/2020                                                                    |
+% |Modified    : 29/12/2020                                                                    |
+% |Description : the function reads the geometry of a propeller and analyzes it using xrotor   |
+% |Reference   : http://web.mit.edu/drela/Public/web/xrotor/                                   |
+% |Input       : standard .txt file that contains informations about propeller geometry        |
+% |Output      : local variables that contain the geometry information about the propeller     |
+%                and/or propeller analysis via interface with xrotor software                  | 
+% |Note        : for the correct use of the tool, user has to put in the same folder:          |
+%                - the matlab file 'main';                                                     |
+%                - the class file 'geometryreader';                                            |
+%                - the xrotor executable;                                                      |
+%                - the standard 'propgeometry.txt' file with the geometry parameters           |           
+% ==============================================================================================
+
 classdef geometryreader < handle
 properties 
 
