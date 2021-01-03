87dd19d47db5b51de430512c33ccb47a2f0577aa
palma <palma@LAPTOP-TNJ0DQQT.lan>
2021-01-03 18:15:57 +0100

    Creation of function for Cd-Cl polar though xfoil

---
+++ CdCl_xfoil.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = CdCl_xfoil(inputArg1,inputArg2)
+%CDCL_XFOIL Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
