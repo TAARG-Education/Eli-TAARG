4cf0765d9204afdfb3fe2929580ef6a9e81db262
Massimiliano <Massimiliano@LAPTOP-JT0Q54HD.lan>
2021-01-15 16:33:03 +0100

    Correzione adimensionalizzazione con il diametro a 1/4 e correzione variabile di output

--- Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello.m
+++ Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello.m
@@ -1,4 +1,4 @@
-function [f] = Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello(ch,Di,De)
+function [fx] = Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello(ch,Di,De)
 %Funzione per il calcolo della velocità indotta da un vortice ad anello. Si
 %utilizza la legge di Biot-Savart per il calcolo delle tre componenti,
 %scritta in un riferimento cilindrico.
@@ -16,26 +16,21 @@ function [f] = Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello(ch,Di,De)
 %Calcoliamo di base le tre quantità integrali
 
 %Gui per l'input da parte dell'utente.
-%  title='Dati Input';
-% prompt={'Lunghezza carenatura[m]','Diametro ingresso [m]','Diametro uscita[m]'};
-% answer=inputdlg(prompt,title,[1 55;1 55;1 55]);
-% ch=str2double(answer{1});
-% Di=str2double(answer{2});
-% De=str2double(answer{3});
+title='Dati Input';
+prompt={'Lunghezza carenatura[m]','Diametro ingresso [m]','Diametro uscita[m]'};
+answer=inputdlg(prompt,title,[1 55;1 55;1 55]);
+ch=str2double(answer{1});
+Di=str2double(answer{2});
+De=str2double(answer{3});
 
-ch=3; %m  
-Di=4.5; %m
-De=1.20; %m
 
-% [f1,c_D14,RatioD,D14,D34] = Universale2(Di,De,ch);
-t=linspace(0,1,100);             %commentare da riga 31 a 40 se si utilizza la funzione 30 per il calcolo dei rapporti c_D14 e RatioD 
+t=linspace(0,1,1000);      
 for i=1:length(t) 
 xs(i)=0+t(i)*(ch);
 ys(i)=Di/2+t(i)*(De/2-Di/2);
 end
-D14=2*ys(25);
-D34=2*ys(75);
-
+D14=2*ys(250);
+D34=2*ys(750);
 c_D14=ch/D14 ;
 RatioD=D34/D14;
 
@@ -51,21 +46,19 @@ Ix=integral(R1,0,2*pi);
 Iy=integral(R2,0,2*pi);
 Iz=integral(R3,0,2*pi);
 %Adimensionalizzazione;
-fx=(r*z*D34/4)*Ix;
-fy=(r*z*D34/4)*Iy;
-fz=(r*D34/4)*Iz;
+fx=(r*z*D14/4)*Ix;
+fy=-(r*z*D14/4)*Iy;
+fz=-(r*D14/4)*Iz;
 %Somma vettoriale;
 f=sqrt(fx^2+fy^2+fz^2);
 
-% P=abs(f1/f-1)*100;
+
 %Grafico in cui ottenere i dati:
-figure(2);
+figure(1);
 plot(f,f)
 axis ([0 1 0 1]);
-text(0,1,'Velocità Indotta');
-% text(0,0.90,['Metodo grafico, f= ',num2str(f1)]);
-text(0,0.80,['Metodo matematico, f= ',num2str(f)]);
-% text(0,0.70,['Errore percentuale, %= ',num2str(P)]);
+text(0.25,1,'Velocità Indotta');
+text(0.25,0.90,['Metodo matematico, fx= ',num2str(fx)]);
 axis off;
 end
 
