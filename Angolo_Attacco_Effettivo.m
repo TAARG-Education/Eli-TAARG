95f47fcdfaae9f40eb6046d58daf1f2e4994e1e2
Utente2019 <Utente2019@DESKTOP-152A7ML.homenet.telecomitalia.it>
2021-01-16 11:48:45 +0100

    Aggiunta documentazione interna.

--- Angolo_Attacco_Effettivo.m
+++ Angolo_Attacco_Effettivo.m
@@ -1,11 +1,65 @@
-function [alpha_e] = Angolo_Attacco_Effettivo(lambda,r_segn,beta,dbeta,mu,psi,theta)
-% ANGOLO_ATTACCO_EFFETTIVO Summary of this function goes here
-%   Detailed explanation goes here
-u_P = zeros(length(r_segn),length(psi));
-u_T = zeros(length(r_segn),length(psi));
-u_R = zeros(length(r_segn),length(psi));
-phi = zeros(length(r_segn),length(psi));
-alpha_e = zeros(length(r_segn),length(psi));
+%% \file Angolo_Attacco_Effettivo
+%  \brief Calculation and visualization of the blade section angle of
+%  attack during a complete rotation of the rotor blade. 
+% \author Serena Manganiello, Michele Massa. 
+% \version 1.0
+%
+% Eli-TAARG is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public
+% License as published by the Free Software Foundation; either
+% version 3 of the License, or (at your option) any later version.
+%
+% Eli-TAARG is developed by the TAARG Educational organization for
+% educational purposes only.
+% Theoretical and Applied Aerodynamic Research Group - University of Naples Federico II.
+%
+% Eli-TAARG GitHub link: <https://github.com/TAARG-Education/Eli-TAARG>
+%
+% Eli-TAARG is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% General Public License for more details.
+% <http://www.gnu.org/licenses/>.
+%
+% ==============================================================================================
+% |Name        : Angolo_Attacco_Effettivo
+% |Author      : Serena Manganiello, Michele Massa.
+% |              University of Naples Federico II.
+% |Version     : 1.0                                                                      
+% |Date        : 01/12/2020                                                            
+% |Modified    : 16/01/2021                                                        
+% |Description : Calculation and visualization of the effective angle of attack
+%                during rotation of the rotor blade.                                             
+% |Reference   : R. Tognaccini. "Lezioni di aerodinamica dell’ala rotante" 2019 pp. 109-112.
+% |Input       : < lambda > rotor inflow ratio.
+%                < r_segn > adimensional coordinate along the rotor blade.
+%                < beta > blade flap angle.
+%                < dbeta > blade flap angle's derivative.
+%                < mu > rotor advance ratio.
+%                < psi > azimuth angle of the blade.
+%                < theta > blade pitch.
+%                < alpha_stall_up > maximum blade section angle of attack.
+%                < alpha_stall_lo > minimum blade section angle of attack.
+% |Output      : blade section angle of attack.
+% |Note        : u_R was calculated to be thorough; stall angles are
+%                imposed in order to visualize only the non-stalled region
+%                of the rotor.
+% ==============================================================================================
+
+function [alpha_e] = Angolo_Attacco_Effettivo(lambda,r_segn,beta,dbeta,mu,psi,theta,alpha_stall_up,alpha_stall_lo)
+
+% Creation of the mesh grid.
+[r_2d,psi_2d] = meshgrid(linspace(0,r_segn(1),length(r_segn)),psi);
+x = r_2d.*cos(psi_2d-pi/2);
+y = r_2d.*sin(psi_2d-pi/2);
+
+% Variables' initialization.
+u_P = zeros(length(r_segn),length(psi)); % air velocity of the blade section, perpendicular to the disk plane.
+u_T = zeros(length(r_segn),length(psi)); % air velocity of the blade section, tangent to the disk plane.
+u_R = zeros(length(r_segn),length(psi)); % radial air velocity of blade section.
+phi = zeros(length(r_segn),length(psi)); % section inflow angle.
+alpha_e = zeros(length(r_segn),length(psi)); % blade section angle of attack.
+
 for i = 1:length(psi)
     u_P(:,i) = lambda + r_segn'.*dbeta(i) + beta(i).*mu.*cos(psi(i));
     u_T(:,i) = r_segn' + mu.*sin(psi(i));
@@ -14,13 +68,11 @@ for i = 1:length(psi)
     alpha_e(:,i) = theta' - phi(:,i);
 end
 
-alpha_stall_1 = convang(20,'deg','rad');
-alpha_stall_2 = convang(-10,'deg','rad');
-stall = zeros(length(r_segn),length(psi));
-non_stall = NaN(length(r_segn),length(psi));
+stall = zeros(length(r_segn),length(psi)); % stalled region of the rotor blade.
+non_stall = NaN(length(r_segn),length(psi)); % non-stalled region of the rotor blade.
 for iii = 1:length(r_segn)
     for jjj = 1:length(psi)
-        if alpha_e(iii,jjj) - alpha_stall_1 > 0 || alpha_e(iii,jjj) - alpha_stall_2 < 0
+        if alpha_e(iii,jjj) - alpha_stall_up > 0 || alpha_e(iii,jjj) - alpha_stall_lo < 0
             stall(iii,jjj) = alpha_e(iii,jjj);
         else
             non_stall(iii,jjj) = alpha_e(iii,jjj);
@@ -28,10 +80,11 @@ for iii = 1:length(r_segn)
     end
 end
 
+% Plot.
 figure(1)
 set(figure(1),'Color','w');
 clf
-fill(x2,y2,'k'); hold on; axis equal; axis off; grid off
+fill(x,y,'k'); hold on; axis equal; axis off; grid off
 text(0.6,0.9,['\mu = ',num2str(mu)],'Color','r','FontSize',16);
 contourf(x,y,convang(non_stall','rad','deg'),25,'ShowText','on'); drawnow; axis equal;
 end
