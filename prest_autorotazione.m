680ab08d53e7b6d4ee07e14ccf36af94133a5d06
acero <acero@PC-Sorekin.home>
2021-01-17 11:54:11 +0100

    creazione funzione

---
+++ prest_autorotazione.m
@@ -0,0 +1,7 @@
+ function [outputArg1,outputArg2] = prest_autorotazione(inputArg1,inputArg2)
+%PREST_AUTOROTAZIONE Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
