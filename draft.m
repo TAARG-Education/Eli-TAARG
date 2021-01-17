1ad9451e68c2d9793ff8f282d51d1fdc45ce0c25
umber <umber@LAPTOP-60CQBBFC>
2021-01-17 18:53:55 +0100

    modifiche allo script draft.m

--- draft.m
+++ draft.m
@@ -1,36 +1,26 @@
 %% test case with Weick 1931 (pag 166 figura C-2)
-clear all; close all; clc
+clear all; close all; clc;
+
 V= 40; %m/s
 rpm= 4500; %giri al minuto
-R_dim= 1.58; %m
-D=1.58*2; %m
+omega= convangvel(rpm, 'rpm', 'rad/s');
 n=rpm/60; %giri al secondo
-%R= 1
-r_hub_dim= 0.14; %m
-r_hub=r_hub_dim/R_dim;
+R= 1.58; %m
+D=1.58*2; %m
+r_hub= 0.14; %m
 N= 3; %numero di pale
 ns= 10; %numero sezioni
-r= linspace(r_hub_dim, 0.97*R_dim, ns);
-r= r/1.58;
-R=R_dim/1.58;
+r= linspace(r_hub, R, ns);
 c= 0.01*[9, 15, 17, 18.5, 19, 19, 17.5, 15.5, 12.5, 9]; %m
-Cl_alpha= ones(1, ns);
-Cl_alpha= 6.28*Cl_alpha;
+Cl_alpha= (2*pi)*ones(1, ns);
 beta_deg= [40, 39.7, 37.6, 32.3, 27.8, 24.4, 21.8, 19.9, 18.3, 17.6];
+beta= convang(beta_deg, 'deg', 'rad');
 x=linspace(r_hub,R,100);
-
-%omega= convangvel(rpm, 'rpm', 'rad/s')
-omega= rpm*2*pi/60;
-i=1;
-while i< ns+1
-beta(i)= convang(beta_deg(i), 'deg', 'rad');
-i= i+1;
-end
+max_iter=1000;
 
 %% Metodo A
-
 lam= V/(omega*R);
-sigma= N*c./(pi*r);
+sigma= N*c./(2*pi*r);
 Vt= omega*r;
 Vr= Vt.*sqrt((lam^2)+ (r.^2));
 phi= atan ((V)./(Vt));
@@ -41,13 +31,7 @@ alpha_i_A= 0.5*(-(lam./r + sigma.*Cl_alpha./(8*r.^2).*(Vr./Vt)) +...
 alpha_i_A_spline=interp1(r,alpha_i_A,x,'spline');
 
 %% Metodo B
-lam= V/(omega*R);
-sigma= N*c./(pi*r);
-Vt= omega.*r;
-Vr= Vt.*sqrt((lam^2)+ (r.^2));
-phi= atan ((V)./(Vt));
-
-F=(2/pi)*acos(exp(N/(2*lam)*(r-R)/R)); %fattore correttivo di Prandtl
+F=(2/pi)*acos(exp(N/(2*lam)*((r-R)/R))); %fattore correttivo di Prandtl
 
 alpha_i_B= 0.5*(-(lam./r + sigma.*Cl_alpha./(8*r.*F.*cos(phi))) ...
          + sqrt((lam./r + sigma.*Cl_alpha./(8*r.*F.*cos(phi))).^2 ...
@@ -55,11 +39,10 @@ alpha_i_B= 0.5*(-(lam./r + sigma.*Cl_alpha./(8*r.*F.*cos(phi))) ...
 alpha_i_B_spline=interp1(r,alpha_i_B,x,'spline');
 
 w_t_B= Vr.*alpha_i_B.*sin(phi+alpha_i_B); % formula C-8
-w_t_C= sigma.*Cl_alpha./(8*r.*F).*Vr.*(beta- phi-alpha_i_B); %%
+w_t_C= sigma.*Cl_alpha./(8*r.*F).*Vr.*(beta- phi-alpha_i_B); %
 w_a_B= Vr.*alpha_i_B.*cos(phi+alpha_i_B); % formula C-9
 
-%% Metodo C (con Newton-Raphson) 
-
+%% Metodo C (con Newton-Raphson)
 alfaisa= alpha_i_B;
 wtsa= w_t_B;
 wasa= w_a_B;
@@ -82,7 +65,7 @@ term2= lam+ sqrt((lam^2)+(4*(w_t_B./Vt).*(r-(w_t_B./Vt)))); %seconda radice di C
 aa= beta-(atan((w_t_B*2)./(term1))); % termine in parentesi nella C-14
 bb= sqrt((0.25*(term2.^2)+((r-w_t_B./Vt)).^2)); %tutto il termine sotto radice della C-17
 cc= 8*r.*F.*w_t_B./Vt; % ha portato al primo membro tutti i termini della C-17 tranne sigma,
-                      % Cl_alpha, aa, bb
+                       % Cl_alpha, aa, bb
 
 da= ((-2)./(((term1).^2)+ 4*w_t_B.*w_t_B)).*(term1...
     -(w_t_B.*(((2*omega*R*r)-(4*w_t_B))./(term1+V))));      %derivata di aa
@@ -94,16 +77,16 @@ dc= 8*r.*F./Vt;  %derivata di cc
 
 y= (sigma.*Cl_alpha.*aa.*bb)- cc; % y indica la differenza tra cc e..
                                   % il resto del termine della C-17
-dy= (sigma.*Cl_alpha.*((aa.*db)+(bb.*da)))-dc;
+dy= (sigma.*Cl_alpha.*((aa.*db)+(bb.*da)))-dc; %derivata di y
 
 i=1;
 
-epsi= 1.e-2; % nel listato fortran vi è un termine 'err' che ...
+epsi= 1.e-5; % nel listato fortran vi è un termine 'err' che ...
              % non è specificato viene preso dall'esterno 
 if abs(y./dy) < epsi
-w_t_B= w_t_B- (y./dy);
+w_t_B= w_t_B-(y./dy);
 else
-    while ((abs(y./dy)> epsi) | i==1001)
+    while ((abs(y./dy)> epsi) | i==max_iter+1)
     
     test1= V^2+ (4*w_t_B.*((omega*R*r)- w_t_B));
     test2= lam^2+ (4*w_t_B./(Vt)).*(r-(w_t_B./(Vt)));
@@ -112,7 +95,7 @@ else
      w_t_B=wtsa;
      w_a_B=wasa;
  else
-        
+
 term1= (sqrt((V^2)+(4*w_t_B.*(omega*R*r- w_t_B)))-V); % 2w_a_C dalla C-15
 term2= lam+ sqrt((lam^2)+(4*(w_t_B./Vt).*(r-(w_t_B./Vt)))); %seconda radice di C-17
 
@@ -135,7 +118,7 @@ dy= (sigma.*Cl_alpha.*((aa.*db)+(bb.*da)))-dc;
 
 i= i+1;
 
-w_t_B= w_t_B-(0.1*w_t_B);
+w_t_B= w_t_B-(y./dy);
 
  end
   
@@ -147,7 +130,7 @@ w_t_B= w_t_B-(0.1*w_t_B);
         alpha_i_C_spline=interp1(r,alpha_i_C,x,'spline');
 end
         
-   if i>1000
+   if i>max_iter
         disp('non ci sono sol per wt')
    end
 end
@@ -180,14 +163,14 @@ delta_cp_c_spline=interp1(r,delta_cp_c,x,'spline');
 %% Grafici 
 figure
 plot(x, alpha_i_A_spline, 'k')
-xlabel('raggio adim');ylabel('\alpha_i [rad]');
+xlabel('r [m]');ylabel('\alpha_i [rad]');
 title ('metodo A')
 
 figure
 plot(x, alpha_i_A_spline, 'k')
 hold on
 plot(x, alpha_i_B_spline, 'b')
-xlabel('raggio adim');ylabel('\alpha_i [rad]');
+xlabel('r [m]');ylabel('\alpha_i [rad]');
 legend('Metodo A','Metodo B')
 title ('metodo A e B a confronto')
 
@@ -197,16 +180,16 @@ hold on
 plot(x, alpha_i_B_spline, 'b')
 hold on
 plot(x, alpha_i_C_spline, 'r')
-xlabel('raggio adim');ylabel('\alpha_i [rad]');
+xlabel('r [m]');ylabel('\alpha_i [rad]');
 legend('Metodo A','Metodo B','Metodo C')
 title ('i 3 metodi a confronto')
 
 figure
 plot(x,delta_ct_a_spline,'k',x,delta_ct_b_spline,'b',x,delta_ct_c_spline,'r')
-xlabel('raggio adim');ylabel('dC_T/dr');
+xlabel('r [m]');ylabel('dC_T/dr');
 legend('Metodo A','Metodo B','Metodo C')
 
 figure
 plot(x,delta_cp_a_spline,'k',x,delta_cp_b_spline,'b',x,delta_cp_c_spline,'r')
-xlabel('raggio adim');ylabel('dC_P/dr');
+xlabel('r [m]');ylabel('dC_P/dr');
 legend('Metodo A','Metodo B','Metodo C')
