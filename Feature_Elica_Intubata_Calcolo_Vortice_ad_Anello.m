f34e95ed573aa7ac0499a3e3229b17a3a8949654
Massimiliano <Massimiliano@LAPTOP-JT0Q54HD.lan>
2021-01-18 10:59:41 +0100

    - Cambio nome in uno più sintetico - aggiunto header - traduzione in inglese - commenti aggiuntivi

--- Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello.m
+++ Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello.m
@@ -1,40 +1,63 @@
-function [fx] = Feature_Elica_Intubata_Calcolo_Vortice_ad_Anello(ch,Di,De)
-%Funzione per il calcolo della velocità indotta da un vortice ad anello. Si
-%utilizza la legge di Biot-Savart per il calcolo delle tre componenti,
-%scritta in un riferimento cilindrico.
-%La legge di Biot-Savart così scritta è: data da
-%                 V(P)=-(Gamma/4*pi)*integ(0,2pi)((R x dl)/R^3).
-%Le tre componenti dimensionali della legge di Biot-Savart nelle tre
-%direzioni vengono così definite:
-%       V_x=(Gamma/4*pi)*r*z*integ(0,2pi)((cos(theta)dtheta)/R^3)   dir x
-%       V_y=-(Gamma/4*pi)*r*z*integ(0,2pi)((sin(theta)dtheta)/R^3)  dir y
-%V_z=-(Gamma/4*pi)*r*integ(0,2pi)(((x-x^)cos(theta)+(y-y^)sin(theta)dtheta)/R^3)
-
-%Si adimensionalizzano le formule per una velocità caratteristica che è la
-%circolazione sul diametro del vortice ad anello definita come
-%Gamma*Dvortice.
-%Calcoliamo di base le tre quantità integrali
-
-%Gui per l'input da parte dell'utente.
-title='Dati Input';
-prompt={'Lunghezza carenatura[m]','Diametro ingresso [m]','Diametro uscita[m]'};
+function [fx] = RVortexInt(ch,Di,De)
+%% \RVortexInt.m
+%  \The function evaluates the dimensionless radial velocity component
+%   of the ring vortex by means of integration of Biot-Savar law
+%  \Olino Massimiliano, Marino Giuseppe
+%  \1.0
+%
+% Eli-TAARG is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public
+% License as published by the Free Software Foundation; either
+% version 3 of the License, or (at your option) any later version.
+%
+% Eli-TAARG is developed by the TAARG Educational organization for
+% educational purposes only.
+% Theoretical and Applied Aerodynamic Research Group - University of Naples Federico II.
+%
+% Eli-TAARG GitHub link: <https://github.com/TAARG-Education/Eli-TAARG>
+%
+% Eli-TAARG is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% General Public License for more details.
+% <http://www.gnu.org/licenses/>.
+%
+% ==============================================================================================
+% |Name        : RVortexInt.m                                                                  |
+% |Author      : Olino Massimiliano, Marino Giuseppe                                           |
+% |              University of Naples Federico II.                                             |
+% |Version     : 1.0                                                                           |
+% |Date        : data di creazione                                                             |
+% |Modified    : data ultima modifica                                                          |
+% |Description : The function evaluates the dimensionless radial velocity component            |
+% |              of the ring vortex by means of integration of Biot-Savar law                  |
+% |Reference   : McCormick, B.W.,(1967), Aerodynamics of V/STOL Flight, Academic Press.        |
+% |              Tognaccini, R., (2020), Lezioni di AERODINAMICA DELL'ALA ROTANTE.             |
+% |Input       : ch (chord), Di (inlet diameter), De (exit diameter) of the shroud             |
+% |Output      : fx (the dimensionless radial velocity component of the ring vortex )          |
+% |Note        :                                                                               |
+% ==============================================================================================
+%
+%Gui for the input by users.
+title='Input Data';
+prompt={'Shroud chord [m]','Inlet diameter [m]','Exit diameter [m]'};
 answer=inputdlg(prompt,title,[1 55;1 55;1 55]);
 ch=str2double(answer{1});
 Di=str2double(answer{2});
 De=str2double(answer{3});
 
-
+% Creation of chroud geometry
 t=linspace(0,1,1000);      
-for i=1:length(t) 
-xs(i)=0+t(i)*(ch);
-ys(i)=Di/2+t(i)*(De/2-Di/2);
-end
-D14=2*ys(250);
-D34=2*ys(750);
+    for i=1:length(t) 
+        xs(i)=0+t(i)*(ch);
+        ys(i)=Di/2+t(i)*(De/2-Di/2);
+    end
+D14=2*ys(250);               % Diameter at 1/4 of the shroud
+D34=2*ys(750);               % Diameter at 3/4 of the shroud
 c_D14=ch/D14 ;
 RatioD=D34/D14;
 
-r=(D14/2);
+r=(D14/2);                   % flow radius
 x=D34/2;
 z=ch*(3/4);
 y=0;
@@ -45,20 +68,22 @@ R3=@(t) ((x-r.*cos(t)).*cos(t)+(y-r.*sin(t)).*sin(t))./(((x-r.*cos(t)).^2 +(y-r.
 Ix=integral(R1,0,2*pi);
 Iy=integral(R2,0,2*pi);
 Iz=integral(R3,0,2*pi);
-%Adimensionalizzazione;
-fx=(r*z*D14/4)*Ix;
+
+% Adimesionalisation of the velocity components
+
+fx=(r*z*D14/4)*Ix;           % Component of interests
 fy=-(r*z*D14/4)*Iy;
 fz=-(r*D14/4)*Iz;
-%Somma vettoriale;
+
+% Vector sum
 f=sqrt(fx^2+fy^2+fz^2);
 
-
-%Grafico in cui ottenere i dati:
+% Results
 figure(1);
 plot(f,f)
 axis ([0 1 0 1]);
-text(0.25,1,'Velocità Indotta');
-text(0.25,0.90,['Metodo matematico, fx= ',num2str(fx)]);
+text(0.25,1,'Velocity induced by vortex ring');
+text(0.25,0.90,['fx= ',num2str(fx)]);
 axis off;
 end
 
