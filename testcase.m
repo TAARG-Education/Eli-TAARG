15d713f40a27e6f501d28de7af38ff6e8f8a4acf
acero <acero@PC-Sorekin.homenet.telecomitalia.it>
2021-01-18 18:02:52 +0100

    Aggiunta test case

---
+++ testcase.m
@@ -0,0 +1,22 @@
+%%% Test case
+%%% Agusta Bell AB 206 Helicopter in forward flight
+% Advance ratio = 0.12
+% Descent angle = 20°
+
+
+% User input
+%%
+clc;clear all;close all
+global M R N c gamma theta_tw f rho      
+
+M = 1120;                             % [Kg], mass
+R = 5.1;                              % [m], rotorblade radius
+N = 2;                                % number of blades
+c = 0.34;                             % [m], chord length
+gamma = 9;                            % Lock's number
+theta_tw = -0.2313;                   % [rad], pitch gradient along the blade (linear variation is assumed)
+f = 0.007;                            % [m^2] equivalent wet area
+rho = 1.225;                          % [Kg/m^3], density (SML)
+
+%% Function output
+[T_C H_C alpha_deg omega V_inf]=prestazioni_autorotazione(0.12,20);
