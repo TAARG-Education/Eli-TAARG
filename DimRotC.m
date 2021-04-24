1c8fb2e91e5a04e54a373e46bbb3235cee789627
verdi <verdi@LAPTOP-A808U2AQ>
2021-04-24 17:59:48 +0200

    funzione inizializzata

--- DimRotC.m
+++ DimRotC.m
@@ -1,7 +1,4 @@
-function [outputArg1,outputArg2] = DimRotC(inputArg1,inputArg2)
-%DIMROTC Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [] = DimRotC()
+%funzione inizializzata
 end
 
