002417861ad29e805e721d3290451062b865a6d3
giamp <giamp@192.168.188.88>
2021-04-24 17:31:19 +0200

    Created DimRotC.m file

---
+++ DimRotC.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = DimRotC(inputArg1,inputArg2)
+%DIMROTC Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
