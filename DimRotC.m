6c9dc6a3119bdcef0c4a7c7e655c905ac6877e17
giamp <giamp@192.168.188.88>
2021-04-24 18:27:24 +0200

    added notes

--- DimRotC.m
+++ DimRotC.m
@@ -1,3 +1,53 @@
+%% \file DimRotC.m
+%  \brief: 
+%
+%  \author: 
+%  \version: 1.00
+%
+% Eli-TAARG is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public
+% License as published by the Free Software Foundation; either
+% version 3 of the License, or (at your option) any later version.
+%
+% Eli-TAARG is developed by the TAARG Educational organization for
+% educational purposes only.
+% Theoretical and Applied Aerodynamic Research Group - University of Naples Federico II.
+%
+% Eli-TAARG GitHub link: <https://github.com/TAARG-Education/Eli-TAARG>
+%
+% Eli-TAARG is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% General Public License for more details.
+% <http://www.gnu.org/licenses/>.
+%
+% ==============================================================================================
+% |Name        : DimRotC.m                                                           |
+% |Author      :                                                         |
+% |              University of Naples Federico II.                                       |
+% |Version     : 1.00                                                                    |
+% |Date        :                                                               |
+% |Modified    :                                                               |
+% |Description :                     |     
+% |Reference   :                                                     |
+% |                          |                                                         |
+% |Input       :                                                                         |
+% |                                                                                  |
+% |                              |  
+% |                                           |
+% |                                            |
+% |                                                 |
+% |                                                |
+% |                                   |
+% |                                   |
+% |                | 
+% |                                |
+% |Output      :                                                                         |
+% |                                                                                      |
+% |                                                                                      |
+% |Note        :                                                                         |
+% ==============================================================================================
+
 function [] = DimRotC()
 %funzione inizializzata
 end
